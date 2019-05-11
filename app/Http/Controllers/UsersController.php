4f35302bc0bd1740ddeec2c08a81b86c63310c41
345111458 <345111458@qq.com>
2019-05-11 07:16:44 +0000

    ;显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,14 +20,16 @@ class UsersController extends Controller
 
 
     public function edit(User $user){
+        $this->authorize('update',$user);
 
 
         return view('users.edit',compact('user'));
     }
 
 
-    public function update(UserRequest $request, User $user)
-    {
+    public function update(UserRequest $request, User $user){
+
+
         $user->update($request->all());
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
