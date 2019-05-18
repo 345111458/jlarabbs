b58e160c7868aa3178af9e6fc7b846be3721b008
345111458 <345111458@qq.com>
2019-05-18 15:58:06 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -21,4 +21,51 @@ class Topic extends Model
 
         return $this->belongsTo(category::class);
     }
+
+
+    public function scopeWithOrder($quest,$order){
+
+        switch($order){
+            case 'recent':
+                $quest->recent();
+                break;
+
+            default:
+                $quest->recentReplied();
+                break;
+
+        }
+
+        // 预加载防止 N+1 问题
+        return $quest->with('user','category');
+    }
+
+
+    public function scopeRecentReplied($quest){
+
+
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $quest->orderBy('updated_at','desc');
+    }
+
+    public function scopeRecent($quest){
+
+        // 按照创建时间排序
+        return $quest->orderBy('created_at','desc');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
